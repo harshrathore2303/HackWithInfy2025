929064aff2ce7b364cad031fe23d1ec304e2c957
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-07-01 09:26:49 +0530

    Activity Selection Problem

--- GreedyApproach/ActivitySelectionProblem.java
+++ GreedyApproach/ActivitySelectionProblem.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MaxPeopleInSingleRoom {
+public class ActivitySelectionProblem {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
